ac41d1daf84549fbb8bb8f124aa7c3e917ef51a1
Yurii Tyshchenko <orty222@gmail.com>
2023-11-10 16:22:58 +0100

    Project setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex flex-col">
+      <span className="text-blue-500 my-2">Typescript</span>
+      <span className="text-green-500 my-2">React</span>
+      <span className="text-red-500 my-2">TailwindCSS</span>
+    </div>
+  );
+}
+
+export default App;
